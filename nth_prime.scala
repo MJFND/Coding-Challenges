b24f040079be7862f405c74e086b3bd87e925aed
Junaid Effendi <jeffendi@earnestresearch.com>
2019-07-04 16:07:59 -0400

    adding more approaches

--- nth_prime.scala
+++ nth_prime.scala
@@ -2,8 +2,10 @@
 // Link: https://blog.junaideffendi.com/
 // Author: Junaid Effendi
 
-// Method 1
+
 val n = 6
+
+// Approach 1
 def nthPrimeNumber(): Array[Int] = {
   var result = Array[Int]()
   for(i <- 1 to 10 * n){
@@ -19,3 +21,56 @@ def nthPrimeNumber(): Array[Int] = {
   return result
 }
 nthPrimeNumber()(n)
+
+// Approach 2
+Using high order functions and code cleaning!
+Passing an integer and returning an integer
+
+def nthPrimeNumber(n: Int) : Int = {
+  val range = (1 to 10 * n).toArray
+  val result = range.map(i => 
+    if((2 until i) forall (i % _ != 0)) i 
+    else 0).filter(_ != 0)
+  return result(n)
+}
+
+nthPrimeNumber(n)
+
+// Approach 3
+def nthPrimeNumber(n: Int) = 
+  (1 to 10 * n).toArray.map(i => 
+    if((2 until i) forall (i % _ != 0)) i 
+    else 0).
+  filter(_ != 0)(n)
+
+nthPrimeNumber(n)
+
+// Approach 4
+def nthPrimeNumber(n: Int) : Int = {
+  var result = Array[Int]()
+  for(i <- 2 to 10 * n){
+    var count = 0
+    for(j <- 2 to i-1){ 
+      if(i % j == 0){
+        count = count + 1
+      }
+    }
+    if (result.length-1 == n) return result(n)
+    if(count == 0 ) result = result :+ i
+  }
+  return 0
+}
+
+nthPrimeNumber(n-1)
+
+// Approach 5
+def nthPrimeNumber(n: Int) : Int = {
+  var count = 0 
+  for(i <- 2 to 10 * n){
+    if((2 until i) forall (i % _ != 0)) count = count + 1
+    if(count-1 == n) return i // return when found
+  }
+  return 0
+}
+
+nthPrimeNumber(n-1)

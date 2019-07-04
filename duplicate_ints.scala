76b677064270f7fc95e75e8e4d294a6ce39ca19e
Junaid Effendi <jeffendi@earnestresearch.com>
2019-07-04 14:13:14 -0400

    fixing indentation

--- duplicate_ints.scala
+++ duplicate_ints.scala
@@ -4,38 +4,38 @@
 
 // Method 1
 def findDups(input: List[Int]) : Set[Int] = {
-	var result = List[Int]()
-	for(i <- 0 to input.length-1){
-		var count = 0
-		for(j <- i+1 to input.length-1){
-			if(input(i) == input(j)){
-				result = result :+ input(i)
-				count = count + 1 
-			}
-		}
-	}
-	result.toSet
+  var result = List[Int]()
+  for(i <- 0 to input.length-1){
+    var count = 0
+    for(j <- i+1 to input.length-1){
+      if(input(i) == input(j)){
+        result = result :+ input(i)
+        count = count + 1 
+      }
+    }
+  }
+  result.toSet
 }
 findDups(List(1,2,3,4,2,5,5,3,5))
 
 // Method 2
 def findDups(input: List[Int]) : Set[Int] = {
-	var result = List[Int]()
-	val sortedInput = input.sorted 
-	for(i <- 0 to sortedInput.length-2){
-		if(sortedInput(i) == sortedInput(i+1))
-			result = result :+ sortedInput(i)
-	}
-	result.toSet
+  var result = List[Int]()
+  val sortedInput = input.sorted 
+  for(i <- 0 to sortedInput.length-2){
+    if(sortedInput(i) == sortedInput(i+1))
+      result = result :+ sortedInput(i)
+  }
+  result.toSet
 }
 findDups(List(1,2,3,4,2,5,5,3,5))
 
 // Method 3
 def findDups(input: List[Int]) : Set[Any] = {
-	input.map(x => 
-		if(input.filter(_ == x).size > 1) x else "").
-		filter(_ != "").
-		toSet
+  input.map(x => 
+    if(input.filter(_ == x).size > 1) x else "").
+    filter(_ != "").
+    toSet
 }
 findDups(List(1,2,3,4,2,5,5,3,5))
 
